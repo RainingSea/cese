8675db5f06e21c227f2149d2410d9acb2da08037
RainingSea <yangrh17@foxmail.com>
2025-01-03 14:37:59 +0800

    update tester, ready to temperature

--- ceaug/ceaug_main.py
+++ ceaug/ceaug_main.py
@@ -3,8 +3,13 @@ import shutil
 from openai import OpenAI
 from utils.read import read_codebase
 from ceaug.auto_test import *
-from ceaug.manipulate import ce_generate
 from utils.read import read_file_2_line
+import re, random
+import ast
+from openai import OpenAI
+import time
+import math
+from datetime import datetime
 
 
 def chat_to_LLM(messages):
@@ -37,7 +42,7 @@ def create_ce_document(project_dir, task_plan, log):
 
     # _______________ generate disturbed plan/ arch / prd of counter example ______________
     # generate 2(default) error(disturbed, whatever) task plan
-    ce_plans = ce_generate(task_plan,log)
+    ce_plans = ce_generate(task_plan, log)
     ce_project_paths = []
     # make a directory for each counter example, and create prd, arch, task plan.
     for i in range(len(ce_plans)):
@@ -158,7 +163,6 @@ def ceaug(base_dir, project_dirs, project_category, project_name, user_req, log)
         messages.append(
             {
                 "role": "user",
-                # "content": "summarize the issues in this project's code based on all the unit test results. Only neIssues about the test codes is not needed to analyze.   ",
                 "content": """Summarize the above mentioned issues or errors. You only need to summarize the issues or errors in the project identified from the unit test result. 
                 Attention: The issues must be exclusively those highlighted by the unit tests; areas that may need improvement (e.g., performance or security concerns) but pass the unit tests should be excluded. Besides, issues result from the test codes is not needed to analyze, only analyze issues that are relevant to the project's own code.
                 Then, you need to provide guidance(Needs to be concise and summarative) for improvement. The guidance you provide should adhere to the following aspects:
@@ -168,6 +172,7 @@ def ceaug(base_dir, project_dirs, project_category, project_name, user_req, log)
             }
         )
 
+        # _______________ 根据单元测试得到的反馈 ______________
         code_feedback = chat_to_LLM(messages)
         log.info(code_feedback)
         print(code_feedback)
@@ -211,6 +216,203 @@ def ceaug(base_dir, project_dirs, project_category, project_name, user_req, log)
 
     return max_score, code_feedback_selected
     # return code_feedback
+
+
+def ce_generate(task_plan, log):
+    # transfer the task plan(dict str) to a dict in python
+    dict_task_plan = ast.literal_eval(task_plan)
+
+    # extract the task list, use re match
+    task_list_dict = extract_task_list(task_plan)
+
+    # assign the number of c.e.
+    ce_number = 1
+    ce_result = []
+    for i in range(ce_number):
+        # avoid affecting the original plan.
+        new_task_plan = dict_task_plan.copy()
+        task_list_dict_copy = task_list_dict.copy()
+        # disturbing, get counter example task list
+        ce = disturbing(task_list_dict_copy, log)
+        # replace the origin task list to disturbed task list
+        new_task_plan["Task list"] = ce
+
+        ce_result.append(str(new_task_plan))
+
+    return ce_result
+
+
+def extract_task_list(task_plan):
+    """
+    accept a task_plan and extract the task_list to a dict, return this dict.
+    """
+
+    # extract *task list* from Task Plan
+    task_list_pattern = r'"Task list":\s*\{(.*?)\},'
+    match = re.search(task_list_pattern, task_plan, re.DOTALL)
+
+    if match:
+        # 提取任务列表
+        task_list_content = match.group(1).strip()
+        # 构造成 JSON 格式（可选）
+        task_list_content = "{" + task_list_content + "}"
+    else:
+        return "Task list not found."
+    # print(task_list_content)
+    # transfer *task list*(string format) to a dict
+    task_dict = ast.literal_eval(task_list_content)
+
+    # 打印结果
+    # print(task_dict)
+    print("Extracted Task Dictionary:")
+
+    _log_task = ""
+    for key, value in task_dict.items():
+        print(f"Key: {key}, Value: {value}")
+        _log_task = _log_task + f"Key: {key}, Value: {value}" + "\n"
+    print()
+    return task_dict
+
+
+def disturbing(task_list_dict, log):
+    """
+    accept a task_dict, apply disturbing strategy to it.
+    """
+    # ________  swap the order of two tasks _________
+    # return perform_swaps(task_dict)
+    # ________  swap the order of two tasks _________
+
+    # _________ remove a task(randomly) _________
+    # return remove_task(task_list_dict)
+    # _________ remove a task(randomly) _________
+
+    # _________ edit a task _________
+    # return edit_task(task_list_dict, log)
+    # _________ edit a task _________
+
+    # ________ no disturbing, just return the original one __________
+    return task_list_dict
+    # ________ no disturbing, just return the original one __________
+
+
+def perform_swaps(d, num_swaps=1):
+    current_dict = d.copy()  # 创建原始字典的一个副本，以避免修改原始字典
+    for _ in range(num_swaps):
+        keys = list(d.keys())
+        if len(keys) < 2:
+            print("The dictionary must contain at least two items to perform a swap.")
+        else:
+            key1, key2 = random.sample(keys, 2)
+            print(key1 + " " + key2)
+            current_dict[key1], current_dict[key2] = (
+                current_dict[key2],
+                current_dict[key1],
+            )
+
+    # 将交换后的列表转换回字典
+
+    return current_dict
+
+
+def remove_task(d):
+    """
+    randomly remove a task
+    """
+    if not d:
+        print("The dictionary is empty, nothing to remove.")
+        return None
+    sample_size = math.ceil(len(d) * 0.3)
+    sample_size = min(sample_size, len(d))
+    keys_to_remove = random.sample(list(d.keys()), sample_size)
+    print("Keys to remove:", str(keys_to_remove))
+    removed_values = [d.pop(key) for key in keys_to_remove]
+    print("Removed values:", str(removed_values))
+
+    return d
+
+
+def edit_task(d, log):
+    """
+    edit one task
+    """
+
+    if not d:
+        print("The dictionary is empty, nothing to remove.")
+        return None
+    # seed
+    seed_value = int(time.time())
+    with open("D:\Project\CE\CE\seed.txt", "a") as file:
+        file.write(str(datetime.now()) + " " + str(seed_value))
+    random.seed(seed_value)
+    #
+    keys = list(d.keys())
+    num_to_select = max(1, round(len(keys) * 0.25))
+    keys_to_edit = random.sample(keys, num_to_select)
+
+    messages = []
+    _log_task = ""
+    print("before edit")
+    for key, value in d.items():
+        print(f"Key: {key}, Value: {value}")
+        _log_task = _log_task + f"Key: {key}, Value: {value}" + "\n"
+
+    template = """Here is a task plan list for implementing a project. I have selected one task, and now I want to create a negative example based on this task. Please make the task I selected more vague.
+    You only need to return an edited task.
+    I will provide you with an example where the task of creating a freely selectable area is omitted, and instead, a fixed area is selected. Please replicate this kind of mistake or ambiguous task behavior.
+    # Example:
+    input:Implement the image cropping feature by allowing users to freely select a region, then crop that region as the output.
+    output:Implement the image cropping feature, allowing users to crop a central region as the result.
+    ---
+    the whole task plan is:{task_plan},
+    the task I selected is:{task},
+    follow instruction and return {num} edited task. 
+    In the output, any unclear tasks or tasks that require special marking should be enclosed within two ***(3-star) symbols, and each task should end with '[end]' to facilitate the distinction between different parts of the task.
+    example:
+    ***
+    T1:vague task1[end]
+    T2:vague task2[end]
+    ...
+    ***
+    """
+    messages.append(
+        {
+            "role": "user",
+            "content": template.format_map(
+                {
+                    "task_plan": _log_task,
+                    "task": str(keys_to_edit),
+                    "num": str(num_to_select),
+                }
+            ),
+        }
+    )
+    edited_tasks = chat_to_LLM(messages)
+    print(edited_tasks)
+    log.info(edited_tasks)
+    matches = re.findall(r"\*\*\*(.*?)\*\*\*", edited_tasks, re.DOTALL)
+    print(matches[0].strip())
+    match = re.search(r"\*\*\*(.*?)\*\*\*", edited_tasks)
+    if match:
+        # 获取被三个星号包围的内容
+        enclosed_content = match.group(1)
+
+        sentences = [
+            sentence.strip()
+            for sentence in enclosed_content.split("[end]")
+            if sentence.strip()
+        ]
+
+        print(sentences)
+        for i in range(len(sentences)):
+            parts = sentences[i].split(":", 1)
+            if len(parts) == 2:
+                vague_key = parts[0].strip()  # 左侧内容
+                vague_task = parts[1].strip()  # 右侧内容
+                d[vague_key] = vague_task
+    else:
+        print("No content found between ***.")
+
+    return d
 
 
 if __name__ == "__main__":
