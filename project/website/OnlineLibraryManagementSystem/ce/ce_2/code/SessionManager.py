497b3cd6932559eda7359e0d108be80a85d115aa
RainingSea <yangrh17@foxmail.com>
2025-05-11 17:48:34 +0800

    local

--- project/website/OnlineLibraryManagementSystem/ce/ce_2/code/SessionManager.py
+++
@@ -1,8 +0,0 @@
-from flask import session
-
-class SessionManager:
-    def is_logged_in(self) -> bool:
-        return 'username' in session
-
-    def get_current_user(self) -> str:
-        return session.get('username', '')
